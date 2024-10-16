fc52e01e88d2ae1cb0fe6a8ba824247c735e4a32
andydangit <andy.dang.it@gmail.com>
2024-10-15 22:10:05 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Andy Dang It on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
