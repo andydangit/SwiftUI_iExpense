acccf9ae9ced06d0b016010d256b565ae119ab15
andydangit <andy.dang.it@gmail.com>
2024-10-17 22:04:07 -0500

    Add: created list to delete from

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -19,25 +19,48 @@ struct SecondView: View {
     }
 }
 
-struct User: Codable {
-    let firstNamed: String
-    let lastNamed: String
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
 }
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 
 struct ContentView: View {
     
-@State private var user = User(firstNamed: "Greatest", lastNamed: "Ever")
+    @State private var expenses = Expenses()
+    
+    // MARK: - View
     
     var body: some View {
-        Button("Save User"){
-            let encoder = JSONEncoder()
-            
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name )
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
             }
         }
-        
     }
+    
+    //MARK: - Function
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
